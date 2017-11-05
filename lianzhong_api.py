873afa416c64092245c8765fb9febb6cdf4142c8
prehawk <prehawk@gmail.com>
2017-11-06 00:08:11 +0800

    登录失败,  但是基本上完成填单操作.

--- lianzhong_api.py
+++ lianzhong_api.py
@@ -16,23 +16,26 @@ def check_points(param):
     r = requests.post('https://v2-api.jsdama.com/check-points', data = json.dumps(param))
     print(json.loads(r.content.decode('utf8')))
 
-def decode_reCaptcha(param, img_b64):
+def decode_reCaptcha(img_b64):
     param['captchaType'] = 1101
     param['captchaMinLength'] = 5
     param['captchaMaxLength'] = 5
     param['captchaData'] = img_b64   
+    url = 'https://v2-api.jsdama.com/upload'
     s = requests.session()
     r = requests.post(url, data = json.dumps(param))
-    print(json.loads(r.content.decode('utf8')))
+    return json.loads(r.content.decode('utf8'))
 
 
 if __name__ == '__main__':
     url = 'https://v2-api.jsdama.com/upload'
     img_b64 = ''
-    with open('data/test.gif', 'rb') as f:
+    with open('bbb.png', 'rb') as f:
         img_content = f.read()
         img_b64 = base64.b64encode(img_content)
         img_b64 = img_b64.decode('ascii')
     
-    check_points(param)
+    #check_points(param)
+    a = decode_reCaptcha(img_b64)
+    print(a)
 
--- simulate.py
+++ simulate.py
@@ -9,13 +9,33 @@ from conf import conf as cf
 import platform
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from PIL import Image
+from lianzhong_api import *
+from io import BytesIO
 
 landing = 'https://online.vfsglobal.com/Global-Appointment/'
 username = cf.register_user
 password = cf.password
 
+def element_screenshot(driver, element, filename):
+    bounding_box = (
+        element.location['x'], # left
+        element.location['y'], # upper
+        (element.location['x'] + element.size['width']), # right
+        (element.location['y'] + element.size['height']) # bottom
+    )
+    return bounding_box_screenshot(driver, bounding_box, filename)
 
+def bounding_box_screenshot(driver, bounding_box, filename):
+    driver.save_screenshot(filename)
+    base_image = Image.open(filename)
+    cropped_image = base_image.crop(bounding_box)
+    base_image = base_image.resize(cropped_image.size)
+    base_image.paste(cropped_image, (0, 0))
+    base_image.save(filename)
+    return base_image
+
+#%%
 #chromedriver = 'C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe'
 chromedriver = 'bin\windows\chromedriver.exe'
 if platform.system() == 'Linux':
@@ -32,3 +52,41 @@ chrome_options.binary_location = r'C:\Program Files (x86)\Google\Chrome\Applicat
 
 
 driver = webdriver.Chrome(chromedriver, chrome_options=chrome_options)
+driver.set_window_size(1440, 900)
+
+
+#%%
+driver.get(landing)
+
+#%%
+# test ok, with  real captcha
+# driver.get_screenshot_as_file('aaa.png')
+
+el_recap = driver.find_element_by_xpath('//*[@id="CaptchaImage"]')
+img_obj = element_screenshot(driver, el_recap, 'bbb.png')
+buffer = BytesIO()
+img_obj.save(buffer, format='PNG')
+img_b64 = base64.b64encode(buffer.getvalue()).decode('ascii')
+
+#%%
+recap = decode_reCaptcha(img_b64)
+print(recap)
+
+#%%
+el_user = driver.find_element_by_xpath('//*[@id="EmailId"]')
+el_user.send_keys(username)
+
+el_password = driver.find_element_by_xpath('//*[@id="Password"]')
+el_password.send_keys(password)
+
+el_recap_input = driver.find_element_by_xpath('//*[@id="CaptchaInputText"]')
+el_recap_input.send_keys(recap['data']['recognition'])
+
+#%%
+el_next = driver.find_element_by_xpath('//*[@id="ApplicantListForm"]/div[4]/input')
+el_next.click()
+
+#%%
+driver.save_screenshot('next.png')
+
+
